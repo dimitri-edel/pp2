aafaaa6c29a42ec892598863b62ed76253100519
dimitri-edel <dimitri.edel@gmx.net>
2023-03-17 12:13:34 +0000

    Add to javscript the five figures of the game

--- assets/jscript/game.js
+++ assets/jscript/game.js
@@ -1,61 +1,109 @@
 /* 
     The user and computer can make a move. 
-    A move uses one of the figures : rock, paper, scissors, lizzard or spock.
-    The both moves will be compared from the user's perspective.
+    A move uses one of the figures : rock, paper, scissors, lizzard or spock.    
 
-    Let's say user picked rock and the computer picked paper. Paper covers rock. So paper wins over rock:
+    Let's say user picked rock and the computer picked paper. 
     +---------------------------+-----------------------------------------+
     |  ROCK FIGURE                                                        |
     +---------------------------+-----------------------------------------+
     | OPOSING INDEX             |  OUTCOME                                |
     +---------------------------+-----------------------------------------+
-    | rock_index                | draw                                    |  Since rock against rock is a draw
+    | versus_rock               | draw                                    |  Since rock against rock is a draw
     +---------------------------+-----------------------------------------+
-    | paper_index               | loss                                    |  Because paper covers rock
+    | versus_paper              | loss                                    |  Because paper covers rock
     +---------------------------+-----------------------------------------+
-    | scissors_index            | win                                     |  Rock crushes scissors
+    | versus_scissors           | win                                     |  Rock crushes scissors
     +---------------------------+-----------------------------------------+
-    | lizzard_index             | win                                     |  Rock crushes lizzard
+    | versus_lizzard            | win                                     |  Rock crushes lizzard
     +---------------------------+-----------------------------------------+
-    | spock_index               | loss                                    |  Spock vaporizes rock
+    | versus_spock              | loss                                    |  Spock vaporizes rock
     +---------------------------+-----------------------------------------+
 
     +---------------------------+-----------------------------------------+
-    |  PAPER FIGURE                                                       |
+    |  PAPER                                                              |
     +---------------------------+-----------------------------------------+
     | OPOSING INDEX             |  OUTCOME                                |
     +---------------------------+-----------------------------------------+
-    | rock_index                | win                                     |  Paper covers rock
+    | versus_rock               | win                                     |  Paper covers rock
     +---------------------------+-----------------------------------------+
-    | paper_index               | draw                                    |  Paper on paper
+    | versus_paper              | draw                                    |  Paper on paper
     +---------------------------+-----------------------------------------+
-    | scissors_index            | loss                                    |  scissors cuts paper
+    | versus_scissors           | loss                                    |  scissors cuts paper
     +---------------------------+-----------------------------------------+
-    | lizzard_index             | loss                                    |  Lizzard eats paper
+    | versus_lizzard            | loss                                    |  Lizzard eats paper
     +---------------------------+-----------------------------------------+
-    | spock_index               | win                                     |  Paper disproves Spock
+    | versus_spock              | win                                     |  Paper disproves Spock
     +---------------------------+-----------------------------------------+
 
     we just asign the outcome of every possible oposing move in a table (array).
     Then we can cross-referrence the outcome by using the index of the oposing move.
 
-    User picked ROCK_FIGURE and the computer picked PAPER_FIGURE = ROCK_FIGURE[paper_index] = loss
+    User picked ROCK and the computer picked PAPER = ROCK [versus_paper] = loss
     Hence, we know that the user has lost. Because paper covers rock.
   
 
 */
 
-/* Indexes of the weights in an array representing the figure */
-const rock_index = 0;
-const paper_index = 1;
-const scissors_index = 2;
-const lizzard_index = 3;
-const spock_index = 4;
+/* Indexes of outcomes */
+const versus_rock = 0;
+const versus_paper = 1;
+const versus_scissors = 2;
+const versus_lizzard = 3;
+const versus_spock = 4;
+const owner_index = 5;
 
 /* Possiple outcomes */
-const win = 1;
-const loss = -1;
-const draw = 0;
+const win = "win";
+const loss = "loss";
+const draw = "draw";
+
+/* 
+    The content of the PAPER figure with possible outcomes at the oposing figure index 
+    owner_index = the own index of the figure, that will be used to determine the outcome.
+*/
+const ROCK = [];
+ROCK[versus_rock] = draw;
+ROCK[versus_paper] = loss;
+ROCK[versus_scissors] = win;
+ROCK[versus_lizzard] = win;
+ROCK[versus_spock] = loss;
+ROCK[owner_index] = versus_rock;
+
+
+/* The content of the PAPER figure with possible outcomes at the oposing figure index */
+const PAPER = [];
+PAPER[versus_rock] = win;
+PAPER[versus_paper] = draw;
+PAPER[versus_scissors] = loss;
+PAPER[versus_lizzard] = loss;
+PAPER[versus_spock] = win;
+PAPER[owner_index] = versus_paper;
+
+
+const SCISSORS = [];
+SCISSORS[versus_rock] = loss;
+SCISSORS[versus_paper] = win;
+SCISSORS[versus_scissors] = draw;
+SCISSORS[versus_lizzard] = win;
+SCISSORS[versus_spock] = loss;
+SCISSORS[owner_index] = versus_scissors;
+
+const LIZZARD = [];
+LIZZARD[versus_rock] = loss;
+LIZZARD[versus_paper] = win;
+LIZZARD[versus_scissors] = loss;
+LIZZARD[versus_lizzard] = draw;
+LIZZARD[versus_spock] = win;
+LIZZARD[owner_index] = versus_lizzard;
+
+const SPOCK = [];
+SPOCK[versus_rock] = win;
+SPOCK[versus_paper] = loss;
+SPOCK[versus_scissors] = win;
+SPOCK[versus_lizzard] = loss;
+SPOCK[versus_spock] = draw;
+SPOCK[owner_index] = versus_spock;
+
 
 
 
